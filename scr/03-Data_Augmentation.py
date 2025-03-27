9885ab45b9f4bbd41462e1273c62119e97e87173
jingaisi <jingaisi@outlook.com>
2025-03-27 00:07:56 +0000

    save csv

--- scr/03-Data_Augmentation.py
+++ scr/03-Data_Augmentation.py
@@ -47,9 +47,16 @@ print(df.head())
 def augment_and_normalize_images(df, output_folder, target_size=(150, 150)):
     # Create an image data generator with augmentation : rotation and flip
     datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=90, horizontal_flip=True)
+    # each item of the dict is a column not a dataframe
+    aug_dict = {
+        "filepath": [],
+        "label": [],
+        "magnification": [],
+        "tumor_subtype": []
+    }
 
     for idx, row in df.iterrows():
-        filepath, label = row["filepath"], row["label"]
+        filepath, label, mag, subtype = row["filepath"], row["label"], row["magnification"], row["tumor_subtype"]
         
         image = cv2.imread(filepath)  # Read image
         if image is None:
@@ -63,16 +70,34 @@ def augment_and_normalize_images(df, output_folder, target_size=(150, 150)):
         x = x.reshape((1,) + x.shape)  # Reshape for ImageDataGenerator
         
         image = image.astype('float32') / 255.0  # Normalize
-        save_path = os.path.join(output_folder, f"original_{idx}.png")
+        save_path_org = os.path.join(output_folder, f"original_{idx}.png")
 
         # save normalized original
         normalized_image = (x[0] * 255).astype(np.uint8)  # Convert back to uint8 for saving
-        cv2.imwrite(save_path, cv2.cvtColor(normalized_image, cv2.COLOR_RGB2BGR))
+        cv2.imwrite(save_path_org, cv2.cvtColor(normalized_image, cv2.COLOR_RGB2BGR))
+        
+        # add the original row in the dict of list (use dict for better efficiency than dataframe)
+        aug_dict["filepath"].append(save_path_org)
+        aug_dict["label"].append(label)
+        aug_dict["magnification"].append(mag)
+        aug_dict["tumor_subtype"].append(subtype)
         
         if label == "malignant":
             continue
         
         # generate one augmented image per original
         for i, batch in enumerate(datagen.flow(x, batch_size=1, save_to_dir=output_folder, save_prefix=f'augmented_{idx}', save_format='png')):
+            save_path_aug = max(glob.glob(f'{output_folder}/augmented_{idx}*.png'), key=os.path.getctime)
+            # add the augmented row in the dict of list (use dict for better efficiency than dataframe)
+            aug_dict["filepath"].append(save_path_aug)
+            aug_dict["label"].append(label)
+            aug_dict["magnification"].append(mag)
+            aug_dict["tumor_subtype"].append(subtype)
             if i == 0:
                 break
+    aug_df = pd.DataFrame(aug_dict)
+    return aug_df
+
+aug_train_df= augment_and_normalize_images(train, "../train_aug")
+
+aug_train_df.to_csv("../data/augmented_train_dataset.csv", index=False)
