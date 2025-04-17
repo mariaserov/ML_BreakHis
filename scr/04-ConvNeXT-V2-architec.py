bab2513601cc0d1058153e0cc65ca981c84639e0
“mariaserov” <serovmari@gmail.com>
2025-04-17 23:09:56 +0100

    Unfreezing test with array jobs on full data

--- scr/04-ConvNeXT-V2-architec.py
+++ scr/04-ConvNeXT-V2-architec.py
@@ -1,5 +1,6 @@
 # Import
 
+import os
 import timm
 import torch
 import torch.nn as nn
@@ -11,6 +12,13 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, random_split, Dataset, random_split
 import pandas as pd
 
+n_epochs = 20
+
+# Array job 
+
+idx = int(os.environ['PBS_ARRAY_INDEX'])
+idx = idx-1 # 0-4
+
 # Set up dataset 
 
 transform = transforms.Compose([
@@ -21,6 +29,7 @@ transform = transforms.Compose([
 ])
 
 # Define custom dataset
+
 
 class BreakHisDataset(Dataset): # Subclass Dataset, which is required for using DataLoader
     def __init__(self, csv_path, transform=None):
@@ -38,6 +47,9 @@ class BreakHisDataset(Dataset): # Subclass Dataset, which is required for using 
             image = self.transform(image)
         return image, torch.tensor(label, dtype=torch.long)
 
+
+## TEMP - UNCOMMENT LATER 
+
 dataset_train = BreakHisDataset(csv_path="../data/augmented_train_dataset.csv", transform=transform) # Load the data
 dataset_test = BreakHisDataset(csv_path="../data/new_test.csv", transform=transform)
 
@@ -46,13 +58,11 @@ dataset_test = BreakHisDataset(csv_path="../data/new_test.csv", transform=transf
 train_loader = DataLoader(dataset_train, batch_size=16, shuffle=True)
 test_loader = DataLoader(dataset_test, batch_size=16, shuffle=False)
 
-# Set up the training loop for different architectures
 
-n_epochs = 10
+# TRAIN THE MODEL: Set up the training loop for different architectures
 
-models = ['Head only', 'Stage 4', 'Stages 3-4', 'Stages 2-4', 'Stages 1-4']
-perf_train = pd.DataFrame(index=range(n_epochs), columns=models)
-perf_test = pd.DataFrame(index=range(n_epochs), columns=models)
+models = ['HeadOnly', 'Stage_4', 'Stages_3-4', 'Stages_2-4', 'Stages_1-4']
+
 
 # Initialise loss criterion & optimiser
 criterion = nn.CrossEntropyLoss()
@@ -61,92 +71,188 @@ criterion = nn.CrossEntropyLoss()
 
 n_stages = 4
 
-for i in range(n_stages+1):
-    
-    # Initialise model
-    
-    model = timm.create_model('convnextv2_atto.fcmae', pretrained=True, num_classes=2)
-    optimiser = optim.Adam(
-        [p for p in model.parameters() if p.requires_grad], 
-        lr=1e-3)
-    
-    for param in model.parameters():
-        param.requires_grad = False
-        
-    for param in model.head.parameters():
-        param.requires_grad = True
-    
-    n_stages_to_unfreeze = i
-    print(f' {i}. Stages to unfreeze: {i}')
-    
-    # Unfreeze stages
-    for a in range(n_stages - n_stages_to_unfreeze, n_stages):
-        for param in model.stages[a].parameters():
-            param.requires_grad = True
-    
-    # Initialise tracking 
-    
-    train_accuracies = []
-    test_accuracies = []
-    train_losses = []
-    test_losses = []
-    
-    for epoch in range(n_epochs):
-        
-        model.train()
-        
-        running_loss = 0
-        correct = 0
-        total = 0
-        
-        for images, labels in train_loader:
-            optimiser.zero_grad()
-            pred = model(images) # forward pass
-            loss = criterion(pred, labels) 
-            loss.backward() # updates model.grad with partial derivatives calc via chain rule
-            optimiser.step() # take a step in negative direction of grad using calculated derivatives
-            
-            running_loss += loss.item() * images.size(0) # Accumulate loss per batch
-            
-            _, preds = torch.max(pred, 1) # Take the maximum one as the class with the highest predicted probability => predicted class
-            
-            correct += (preds == labels).sum().item()
-            total += labels.size(0)
-        
-        epoch_train_loss = running_loss/total
-        train_losses.append(epoch_train_loss)
-        
-        epoch_train_accuracy = correct/total 
-        train_accuracies.append(epoch_train_accuracy)
-        
-        model.eval()
-        
-        running_loss_test = 0
-        correct_test = 0
-        total_test = 0
-        
-        with torch.no_grad():
-            for images, labels in test_loader:
-                pred = model(images)
-                loss = criterion(pred, labels)
-                running_loss_test += loss.item() * images.size(0)
-                _, preds = torch.max(pred,1)
-                
-                correct_test += (preds == labels).sum().item()
-                total_test += labels.size(0)
-            
-            
-        test_losses.append(running_loss_test/total_test)
-            
-        epoch_test_accuracy = correct_test/total_test
-        test_accuracies.append(epoch_test_accuracy)
-        
-        print(f"Epoch {epoch+1}/{n_epochs}: Train Accuracy {epoch_train_accuracy}, Test Accuracy {epoch_test_accuracy}")
-    
-    perf_train.iloc[:,i] = train_accuracies
-    perf_test.iloc[:,i] = test_accuracies
+# NON-PARALLELISED LOOP 
 
-perf_train.to_csv('convnext_v2_outputs/arc_perf_train.csv')
-perf_test.to_csv('convnext_v2_outputs/arc_perf_test.csv')
+# perf_train = pd.DataFrame(index=range(n_epochs), columns=models)
+# perf_test = pd.DataFrame(index=range(n_epochs), columns=models)
+
+# for i in range(n_stages+1):
+    
+#     # Initialise model
+    
+#     model = timm.create_model('convnextv2_atto.fcmae', pretrained=True, num_classes=2)
+#     optimiser = optim.Adam(
+#         [p for p in model.parameters() if p.requires_grad], 
+#         lr=1e-3)
+    
+#     for param in model.parameters():
+#         param.requires_grad = False
+        
+#     for param in model.head.parameters():
+#         param.requires_grad = True
+    
+#     n_stages_to_unfreeze = i
+#     print(f' {i}. Stages to unfreeze: {i}')
+    
+#     # Unfreeze stages
+#     for a in range(n_stages - n_stages_to_unfreeze, n_stages):
+#         for param in model.stages[a].parameters():
+#             param.requires_grad = True
+    
+#     # Initialise tracking 
+    
+#     train_accuracies = []
+#     test_accuracies = []
+#     train_losses = []
+#     test_losses = []
+    
+#     for epoch in range(n_epochs):
+        
+#         model.train()
+        
+#         running_loss = 0
+#         correct = 0
+#         total = 0
+        
+#         for images, labels in train_loader:
+#             optimiser.zero_grad()
+#             pred = model(images) # forward pass
+#             loss = criterion(pred, labels) 
+#             loss.backward() # updates model.grad with partial derivatives calc via chain rule
+#             optimiser.step() # take a step in negative direction of grad using calculated derivatives
+            
+#             running_loss += loss.item() * images.size(0) # Accumulate loss per batch
+            
+#             _, preds = torch.max(pred, 1) # Take the maximum one as the class with the highest predicted probability => predicted class
+            
+#             correct += (preds == labels).sum().item()
+#             total += labels.size(0)
+        
+#         epoch_train_loss = running_loss/total
+#         train_losses.append(epoch_train_loss)
+        
+#         epoch_train_accuracy = correct/total 
+#         train_accuracies.append(epoch_train_accuracy)
+        
+#         model.eval()
+        
+#         running_loss_test = 0
+#         correct_test = 0
+#         total_test = 0
+        
+#         with torch.no_grad():
+#             for images, labels in test_loader:
+#                 pred = model(images)
+#                 loss = criterion(pred, labels)
+#                 running_loss_test += loss.item() * images.size(0)
+#                 _, preds = torch.max(pred,1)
+                
+#                 correct_test += (preds == labels).sum().item()
+#                 total_test += labels.size(0)
+            
+            
+#         test_losses.append(running_loss_test/total_test)
+            
+#         epoch_test_accuracy = correct_test/total_test
+#         test_accuracies.append(epoch_test_accuracy)
+        
+#         print(f"Epoch {epoch+1}/{n_epochs}: Train Accuracy {epoch_train_accuracy}, Test Accuracy {epoch_test_accuracy}")
+    
+#     perf_train.iloc[:,i] = train_accuracies
+#     perf_test.iloc[:,i] = test_accuracies
+
+# PARALLELISED
+experiment = models[idx]
+
+perf_train = pd.DataFrame(index=range(n_epochs), columns=[experiment])
+perf_test = pd.DataFrame(index=range(n_epochs), columns=[experiment])
+
+# Initialise model
+    
+model = timm.create_model('convnextv2_atto.fcmae', pretrained=True, num_classes=2)
+
+for param in model.parameters():
+    param.requires_grad = False
+    
+for param in model.head.parameters():
+    param.requires_grad = True
+
+n_stages_to_unfreeze = idx
+print(f' {idx}. Stages to unfreeze: {idx}')
+
+# Unfreeze stages
+for a in range(n_stages - n_stages_to_unfreeze, n_stages):
+    for param in model.stages[a].parameters():
+        param.requires_grad = True
+
+optimiser = optim.Adam(
+    [p for p in model.parameters() if p.requires_grad], 
+    lr=1e-3)
+
+# Initialise tracking 
+
+train_accuracies = []
+test_accuracies = []
+train_losses = []
+test_losses = []
+
+for epoch in range(n_epochs):
+    
+    model.train()
+    
+    running_loss = 0
+    correct = 0
+    total = 0
+    
+    for images, labels in train_loader:
+        optimiser.zero_grad()
+        pred = model(images) # forward pass
+        loss = criterion(pred, labels) 
+        loss.backward() # updates model.grad with partial derivatives calc via chain rule
+        optimiser.step() # take a step in negative direction of grad using calculated derivatives
+        
+        running_loss += loss.item() * images.size(0) # Accumulate loss per batch
+        
+        _, preds = torch.max(pred, 1) # Take the maximum one as the class with the highest predicted probability => predicted class
+        
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+    
+    epoch_train_loss = running_loss/total
+    train_losses.append(epoch_train_loss)
+    
+    epoch_train_accuracy = correct/total 
+    train_accuracies.append(epoch_train_accuracy)
+    
+    model.eval()
+    
+    running_loss_test = 0
+    correct_test = 0
+    total_test = 0
+    
+    with torch.no_grad():
+        for images, labels in test_loader:
+            pred = model(images)
+            loss = criterion(pred, labels)
+            running_loss_test += loss.item() * images.size(0)
+            _, preds = torch.max(pred,1)
+            
+            correct_test += (preds == labels).sum().item()
+            total_test += labels.size(0)
+        
+        
+    test_losses.append(running_loss_test/total_test)
+        
+    epoch_test_accuracy = correct_test/total_test
+    test_accuracies.append(epoch_test_accuracy)
+    
+    print(f"Epoch {epoch+1}/{n_epochs}: Train Accuracy {epoch_train_accuracy}, Test Accuracy {epoch_test_accuracy}")
+
+perf_train.loc[:,experiment] = train_accuracies
+perf_test.loc[:,experiment] = test_accuracies
+
+perf_train.to_csv(f'convnext_v2_outputs/array_job_output/{experiment}_train.csv')
+perf_test.to_csv(f'convnext_v2_outputs/array_job_output/{experiment}_test.csv')
+
 print(perf_train)
 print(perf_test)
--- scr/04-ConvNeXT-V2-architec.sh
+++ scr/04-ConvNeXT-V2-architec.sh
@@ -1,10 +1,11 @@
 #PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=1:mem=50gb
 #PBS -N ConvNeXTV2
+#PBS -J 1-5
 
 cd /rds/general/user/ms7024/home/ML_BreakHis/scr
 
 module load anaconda3/personal
 source activate breakhis
 
-python 04-ConvNeXT-V2-architec.py
+python 04-ConvNeXT-V2-architec.py $PBS_ARRAY_INDEX
