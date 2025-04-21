e40c8c86a252a1366cc18867014ae316600d4013
“mariaserov” <serovmari@gmail.com>
2025-04-21 15:02:52 +0100

    Convnext-V2 final model binary

---
+++ scr/04g-ConvNeXT-V2_final_binary.sh
@@ -0,0 +1,10 @@
+#PBS -l walltime=8:00:00
+#PBS -l select=1:ncpus=1:mem=300gb
+#PBS -N ConvnextV2_FinalModel_Binary
+
+cd /rds/general/user/ms7024/home/ML_BreakHis/scr
+
+module load anaconda3/personal
+source activate breakhis
+
+python 04g-ConvNeXT-V2_final_binary.py $PBS_ARRAY_INDEX
